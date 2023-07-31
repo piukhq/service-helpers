5a1cb99dcf4840dc6b8ba57eef8dac374f105e57
Chris Pressland <mail@cpressland.io>
2023-07-31 12:53:49 +0100

    Chromedriver fixes

--- service_helpers/chromedriver.py
+++ service_helpers/chromedriver.py
@@ -7,22 +7,29 @@ import requests
 
 def chromedriver(path):
     c_version = str(os.popen("mdls -raw -name kMDItemVersion '/Applications/Google Chrome.app'").read())
-    c_major_version = str(c_version).split(".")[0]
     print(f"Detected Chrome version {c_version}")
-    d_version = requests.get(f"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{c_major_version}").text
-    print(f"Best driver version candidate: {d_version}")
-    d_filename = "chromedriver_mac_arm64.zip" if platform.machine() == "arm64" else "chromedriver_mac64.zip"
-    d_url = f"https://chromedriver.storage.googleapis.com/{d_version}/{d_filename}"
-    print(f"Downloading {d_url}")
-    with requests.get(d_url, stream=True) as r:
+    c_semver = ".".join(c_version.split(".")[:3])
+    available_versions = requests.get(
+        "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json"
+    ).json()
+    best_version = [version for version in available_versions["versions"] if c_semver in version["version"]][-1]
+    print(f"Best Chrome Driver Version: {best_version['version']}")
+    platform_type = "mac-arm64" if platform.processor() == "arm" else "mac-x64"
+    url = next(
+        download for download in best_version["downloads"]["chromedriver"] if download["platform"] == platform_type
+    )["url"]
+    with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open("/tmp/chromedriver.zip", "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
 
-    print(f"Unzipping {d_filename} to {path}")
+    print(f"Unzipping chromedriver to {path}")
     with zipfile.ZipFile("/tmp/chromedriver.zip", "r") as z:
-        z.extractall(path)
+        for file in z.infolist():
+            file.filename = os.path.basename(file.filename)
+            if file.filename == "chromedriver":
+                z.extract(file, path=path)
 
     print(f"Fixing permissions on {path}/chromedriver")
     os.chmod(f"{path}/chromedriver", 0o755)

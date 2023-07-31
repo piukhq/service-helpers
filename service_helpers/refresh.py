6aa8f3e808a897ea88d2bce94818fe9ad143760f
Chris Pressland <mail@cpressland.io>
2023-07-31 12:53:49 +0100

    Chromedriver fixes

--- service_helpers/refresh.py
+++ service_helpers/refresh.py
@@ -1,11 +1,10 @@
 import csv
 import os
 
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
-from selenium import webdriver
 
 
 def setup_chromedriver():
